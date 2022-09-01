280f24bc0a35a53972f0d132cb1e900e3d6767a2
Robert-Ro <liutsingluo@gmail.com>
2022-09-02 01:45:05 +0800

    2 files modified, 1 deleted

--- main/src/App.js
+++ main/src/App.js
@@ -4,30 +4,32 @@ import Garfish from 'garfish';
 const App = () => {
   useEffect(async () => {
     // docs: https://garfish.top/
-    const app = await Garfish.loadApp('app2', {
-      entry: 'http://localhost:3002',
-      basename: '/',
-      domGetter: '#container',
-      sandbox: {
-        fixBaseUrl: true,
-      },
-      props: {
-        msg: 'hello world',
-      },
-    });
-    await app.mount();
-    const app3 = await Garfish.loadApp('app3', {
-      entry: 'http://localhost:3003',
-      basename: '/',
-      domGetter: '#container2',
-      sandbox: {
-        fixBaseUrl: true,
-      },
-      props: {
-        msg: 'hello world',
-      },
-    });
-    await app3.mount();
+    const [app, app3] = await Promise.all([
+      Garfish.loadApp('app2', {
+        entry: 'http://localhost:3002',
+        basename: '/',
+        domGetter: '#container',
+        sandbox: {
+          fixBaseUrl: true,
+        },
+        props: {
+          msg: 'hello world',
+        },
+      }),
+      Garfish.loadApp('app3', {
+        entry: 'http://localhost:3003',
+        basename: '/',
+        domGetter: '#container2',
+        sandbox: {
+          fixBaseUrl: true,
+        },
+        props: {
+          msg: 'hello world',
+        },
+      }),
+    ]);
+    app.mount();
+    app3.mount();
   });
 
   return (
@@ -43,7 +45,7 @@ const App = () => {
     >
       <h1>Main App</h1>
       <div id="container"></div>
-      <hr />
+
       <div id="container2"></div>
     </div>
   );
