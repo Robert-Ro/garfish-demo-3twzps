d8360cde144047561fb96478c92988ec026242f2
Liutsing <liutsingluo@163.com>
2022-09-12 20:23:11 +0800

    use workspace lib

--- packages/sub/src/App.js
+++ packages/sub/src/App.js
@@ -1,55 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
-
-function Home({ msg }) {
-  return <div>home msg: {msg}</div>;
-}
+import React from "react";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { getWeekNumber } from "@liutsing/utils";
 
 const App = ({ props }) => {
   return (
     <BrowserRouter>
-      <div
-        style={{
-          margin: '10px',
-          padding: '10px',
-          textAlign: 'center',
-          backgroundColor: 'cyan',
-        }}
-      >
-        <h1>Sub App</h1>
-        <nav
-          style={{
-            borderBottom: 'solid 1px',
-            paddingBottom: '1rem',
-          }}
-        >
-          <Link to="/">Home</Link> | <Link to="/invoices">Invoices</Link> |{' '}
-          <Link to="/expenses">Expenses</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home msg={props.msg} />} />
-          <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />} />
-        </Routes>
+      <div>
+        <h1>Sub App Test App</h1>
+        <div>Current Week No.{getWeekNumber()}</div>
       </div>
     </BrowserRouter>
   );
 };
-
-function Expenses() {
-  return (
-    <main style={{ padding: '1rem 0' }}>
-      <h2>Expenses</h2>
-    </main>
-  );
-}
-
-function Invoices() {
-  return (
-    <main style={{ padding: '1rem 0' }}>
-      <h2>Invoices</h2>
-    </main>
-  );
-}
 
 export default App;
